787fcdf062b28ba6682f20990ee258073f533625
WhoIAm-Benjamin <76595934+WhoIAm-Benjamin@users.noreply.github.com>
2020-12-25 21:12:14 +0300

    Add files via upload
    Основные файлы добавлены

---
+++ GUI.py
@@ -0,0 +1,45 @@
+from PySide2 import QtWidgets, QtCore
+import design
+
+
+class ExampleApp(QtWidgets.QMainWindow, design.Ui_MainWindow):
+    content = []
+    def __init__(self):
+        # доступ к переменным в файле design.py
+        super().__init__()
+        self.setupUi(self) # инициализация интерфейса
+        self.addButton.clicked.connect(self.adding) # кнопка добавления
+        self.clearButton.clicked.connect(self.clear_space) # кнопка удаления
+        self.customContextMenuRequested.connect(self.show_menu) # контекстное меню
+
+    # noinspection PyArgumentList
+    def adding(self):
+        name = str(self.nameEvent.displayText())
+        time_and_date = str(self.timeAndDate.dateTime().toString('HH:mm dd-MM-yyyy'))
+        string = '{} в {}'.format(name, time_and_date)
+        if len(string) > 0:
+            if string not in self.content:
+                string = str(len(self.content) + 1) + '. ' + string
+                print(string)
+                self.myList.addItem(string)
+                self.content.append(string)
+                self.clear_space()
+        else:
+            pass
+
+    def clear_space(self):
+        self.nameEvent.setText('')
+        time = QtCore.QDateTime.currentDateTime()
+        self.timeAndDate.setDateTime(time)
+
+
+def main():
+    app = QtWidgets.QApplication() # новый экземпляр QApplication
+    window = ExampleApp() # новый  объект класса ExampleApp
+    window.show() # показываем окно
+    app.exec_() # запускаем приложение
+
+
+
+if __name__ == '__main__':
+    main()
